be2310157f2b28d6482823559e52a88598ed39e8
chazzka <adamo.ulrich@gmail.com>
2022-04-22 15:12:15 +0200

    commitfsda

--- try.jl
+++ try.jl
@@ -5,3 +5,13 @@ end
 function remove(a, b)
     return a - b
 end
+
+function isEven(x)
+    return x % 2 == 0
+end
+
+function onlyEven(x)
+    return filter(isEven, x)
+end
+
+print(onlyEven([1,2,3,4]))
--- try_test.jl
+++ try_test.jl
@@ -4,4 +4,6 @@ include("try.jl")
 
 @test true
 
-@test add(5,5) != 10
+@test add(5,5) == 10
+
+@test onlyEven([1,2,3,4]) == [2,4]
